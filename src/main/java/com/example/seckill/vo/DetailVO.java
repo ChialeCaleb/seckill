37ccaf01e942055a3ab1b60e97ed1ac9e2f74a8d
ChialeCaleb <chialecaleb@qq.com>
2023-04-01 19:08:57 +0800

    页面优化

---
+++ src/main/java/com/example/seckill/vo/DetailVO.java
@@ -0,0 +1,13 @@
+package com.example.seckill.vo;
+
+public class DetailVO {
+
+    private TUser tUser;
+
+    private GoodsVo goodsVo;
+
+    private int secKillStatus;
+
+    private int remainSeconds;
+
+}
